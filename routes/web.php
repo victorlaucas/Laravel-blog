3c9d35eb64b69dc906228f2881cc17c751b3d552
Victor <victorlaucas@gmail.com>
2017-12-05 10:05:30 -0700

    Generated controller and methods associated with route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
-use App\Task;
 
-Route::get('/tasks', function () {
-  $tasks = Task::all();
-  return view('tasks.index', compact('tasks'));
-});
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/tasks/{task}', function ($id) {
-  $task = Task::find($id);
-  return view('tasks.show', compact('task'));
-});
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('about', function(){
     return view('about');
