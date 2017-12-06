26556f65f78ece93619215465ffcfd41d9987a8a
Victor <victorlaucas@gmail.com>
2017-12-06 15:30:18 -0700

    Archives. Sidebar has dynamic links to query months based on creation date

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
@@ -11,8 +12,17 @@ class PostsController extends Controller
     }
 
     public function index(){
-      $posts = Post::latest()->get();
-      return view('posts.index', compact('posts'));
+      $posts = Post::latest()
+        ->filter(request(['month', 'year']))
+        ->get();
+
+      $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+        ->groupBy('year', 'month')
+        ->orderByRaw('min(created_at) desc')
+        ->get()
+        ->toArray();
+
+      return view('posts.index', compact('posts', 'archives'));
     }
 
     public function show(Post $post){
--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
 class Post extends Model
 {
     public function comments(){
@@ -18,5 +20,20 @@ class Post extends Model
 
     public function user(){
       return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter($query, $filters){
+
+      if (isset($filters['month'])){
+        if($month = $filters['month']){
+          $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+      }
+
+      if(isset($filters['year'])){
+        if($year = $filters['year']){
+          $query->whereYear('created_at', $year);
+        }
+      }
     }
 }
