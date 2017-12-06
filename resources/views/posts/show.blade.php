2b018fd4d40a715adfb76d2e6845b9c80ffa0fca
Victor <victorlaucas@gmail.com>
2017-12-06 09:57:14 -0700

    Created comments controller. Added comment form to write comments from show page. Form comment body validations

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,12 +19,32 @@
               </strong>
 
               {{ $comment->body }}
-              
+
             </li>
           @endforeach
         </ul>
       </div>
+      <hr>
+      {{-- Add a comment --}}
+      <div class="card">
+        <div class="card-block">
+          <form method="POST" action="/posts/{{ $post->id }}/comments">
 
+            {{ csrf_field() }}
+
+            <div class="form-group">
+              <textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary">Add Comment</button>
+            </div>
+          </form>
+
+          @include('layouts.errors')
+
+        </div>
+      </div>
     </div>
   </div>
 @endsection
