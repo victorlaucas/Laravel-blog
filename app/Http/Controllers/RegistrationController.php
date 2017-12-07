2f8e146c5161486d87045a3a1dd8746a1910b4c4
Victor <victorlaucas@gmail.com>
2017-12-07 12:46:57 -0700

    Form Requests and Form Objects. Not very common or always needed, but can be useful

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -6,20 +6,16 @@ use App\User;
 
 use App\Mail\Welcome;
 
+use App\Http\Requests\RegistrationRequest;
+
 class RegistrationController extends Controller
 {
     public function create(){
       return view('registration.create');
     }    
 
-    public function store(){
-      // Validate the form
-      $this->validate(request(), [
-        'name' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|confirmed'
+    public function store(RegistrationRequest $request){
 
-      ]);
 
       $user = User::create(request(['name', 'email', 'password']));
 
