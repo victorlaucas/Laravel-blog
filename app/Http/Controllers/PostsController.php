404317cb42eb3a7a2a8cb75da7b0c4d2041850b1
Victor <victorlaucas@gmail.com>
2017-12-06 22:32:04 -0700

    View Composers. Note: did not implement fully because i dont have a sidebar on every page. when multiple views require the same piece of data you will use a view composer

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -16,11 +16,7 @@ class PostsController extends Controller
         ->filter(request(['month', 'year']))
         ->get();
 
-      $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-        ->groupBy('year', 'month')
-        ->orderByRaw('min(created_at) desc')
-        ->get()
-        ->toArray();
+      $archives = Post::archives();
 
       return view('posts.index', compact('posts', 'archives'));
     }
